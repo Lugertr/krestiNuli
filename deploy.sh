0af3eefd52a8f88621b88391aa3e07992abb1ef4
Lugertr <nikitavishnev@bk.ru>
2022-10-07 03:57:10 +0300

    qwe

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 #git push -f git@github.com:Lugertr/krestynuli.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Lugertr/krestynuli.git master:gh-pages
+git push -f https://github.com/Lugertr/krestynuli.git master:gh-pages
 
 cd -
